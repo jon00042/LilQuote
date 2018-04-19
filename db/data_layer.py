b66a0a0381b4a8fe869d43fe9ee0ae05bcc1de36
Jon Leichter <jon00042@gmail.com>
2018-04-19 15:44:21 +0800

    Login working

--- db/data_layer.py
+++ db/data_layer.py
@@ -11,7 +11,7 @@ def get_all_quotes_for(user_id):
     pass
 
 def search_by_user_or_email(query):
-    return db.open().query(User).filter(or_(User.username.like('%{}%'.format(query)), User.email.like('%{}%'.format(query)))).all()
+    return db.open().query(User).filter(or_(User.fullname.like('%{}%'.format(query)), User.email.like('%{}%'.format(query)))).all()
 
 def create_quote(user_id, content):
     pass
@@ -22,13 +22,18 @@ def delete_quote(quote_id):
 def get_user_by_id(user_id):
     pass
 
-def get_user_by_name(username):
+def get_user_by_name(fullname):
     pass
 
-def get_user_by_email(user_email):
-    pass
+def get_user_by_email(email):
+    return db.open().query(User).filter(User.email == email).one();
 
-def create_user(email, username, password):
-    pass
+def create_user(email, fullname, password):
+    user = User()
+    user.email = email
+    user.fullname = fullname
+    user.password = password
+    db.save(user)
+    return user
 
 
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import re
+import sqlalchemy
 from flask import Flask, session, request, redirect, render_template, flash, url_for
+from db.data_layer import create_user, get_user_by_email
 
-import db.data_layer as db
+# import db.data_layer as db
 '''
 USAGE:        db.<function_name>
 EXAMPLES:     db.search_by_user_or_email('Smith')
@@ -28,7 +30,7 @@ def delete_quote(quote_id):
 @app.route('/search')
 def search():
     return redirect(url_for('search_users', query=request.args['html_query']))
-    
+
 @app.route('/results/<query>')
 def search_users(query):
     pass
@@ -37,24 +39,69 @@ def search_users(query):
 def user_quotes(user_id):
     pass
 
-@app.route('/authenticate')
-def authenticate():
-    pass
+@app.route('/register_form')
+def register_form():
+    if ('user_id' in session):  # in case someone navigates manually
+        return redirect(url_for('index'))
+    return render_template('register_form.html')
+
+@app.route('/login_form')
+def login_form():
+    if ('user_id' in session):  # in case someone navigates manually
+        return redirect(url_for('index'))
+    return render_template('login_form.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    pass
+    if (len(request.form['email']) <= 0 or len(request.form['password']) <= 0):
+        flash('login fields cannot be empty!')
+        return redirect(url_for('login_form'))
+    user = None
+    try:
+        user = get_user_by_email(request.form['email'])
+    except sqlalchemy.orm.exc.NoResultFound:
+        flash('failed login attempt!')
+    except Exception as ex:
+        flash('internal error: {}'.format(ex))
+    if (user is None):
+        return redirect(url_for('login_form'))
+    if (user.password != request.form['password']):
+        flash('failed login attempt!')
+        return redirect(url_for('login_form'))
+    session['user_id'] = user.id
+    session['fullname'] = user.fullname
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['POST'])
 def register():
-    pass
+    is_valid = True
+    for field in [ 'email', 'fullname', 'password' ]:
+        if (len(request.form[field]) <= 0):
+            flash('{} cannot be empty!'.format(field))
+            is_valid = False
+    if (request.form['password'] != request.form['confirm']):
+        flash('passwords do not match!')
+        is_valid = False
+    if (not is_valid):
+        return redirect(url_for('register_form'))
+    user = None
+    try:
+        user = create_user(request.form['email'], request.form['fullname'], request.form['password'])
+    except sqlalchemy.exc.IntegrityError:
+        flash('email address already registered!')
+    except Exception as ex:
+        flash('internal error: {}'.format(ex))
+    print('user: {}'.format(user))
+    if (user is None):
+        return redirect(url_for('register_form'))
+    session['user_id'] = user.id
+    session['fullname'] = user.fullname
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    pass
-
-def setup_web_session(user):
-    pass
-
+    session.clear()
+    return redirect(url_for('index'))
 
 app.run(debug=True)
+
